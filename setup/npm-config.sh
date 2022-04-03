63ff78eed3705ee6e3037a2740bd313100a59b69
Vladimir Vic. Pavlychev <vladimir.keyjoo@gmail.com>
2022-04-04 00:00:37 +0300

    :rocket: Initial |> Setup

---
+++ setup/npm-config.sh
@@ -0,0 +1,8 @@
+!#/bin/bash
+# 2022-04-03
+
+npm config set init.author.name "Vladimir Victorovich Pavlychev "
+npm config set init.author.email "vladimir.keyjoo@gmail.com"
+npm config set init.author.url "https://osarts.ru/ceo/"
+npm config set init.license "MIT"
+npm config set init.version "0.0.1"
